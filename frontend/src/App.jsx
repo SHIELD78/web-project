73e88416db3ac14f8731e8a91af124fbd87807d3
harshdalmia <harshdalmia08@gmail.com>
2025-03-29 21:40:22 +0530

    added pages and components folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import SignUpPage from "./Pages/SignUp";
+import SignUpPage from "./Pages/SignUp/SignUp";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/" element={<HeroPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/selectorg" element={<SelectOrgPage />} />
+        <Route path="/activitylog" element={<ActivityLogPage />} />
+        <Route path="/activitylog/organization/:orgId" element={<ActivityLogPage />} />
+        <Route path="/activitylog/board/:boardId" element={<ActivityLogPage />} />
+        <Route path="/activitylog/task/:taskId" element={<ActivityLogPage />} />
+        <Route path="/board/:boardId" element={<BoardPage />} />
+        <Route path="/task/:taskId" element={<TaskPage />} />
       </Routes>
     </Router>
   );
